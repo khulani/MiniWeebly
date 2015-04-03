a4db6feead08c85eb4f733ee9917be689ae0af6f
Khulani Malone <khulani@gmail.com>
2015-04-03 14:13:36 -0700

    attempting heroku deployment with google login

---
+++ config/initializers/omniauth-local.rb
@@ -0,0 +1,5 @@
+OmniAuth.config.logger = Rails.logger
+
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :google_oauth2, '258791180938-n9fnia1mo156mnbmsrt7jk2gf92a2f3n.apps.googleusercontent.com', 'dMWhC3dpGtpkoBHsUW-1esfX', {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}}
+end
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, '258791180938-jk0ave4al2kuccevhjdsn9bkiu0a9a9b.apps.googleusercontent.com', 'gWe1G2KsJWpVfFaOBnr8ooBv', {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}}
+  provider :google_oauth2, '258791180938-egaaids9c2irqcaren6nc0brpt14pivg.apps.googleusercontent.com', 'Iww2u4nRh6pgYq_v8gnz7_CD', {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}}
 end
